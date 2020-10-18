c0558a991800284d3cf65d501e1f4bcd42bb2c9f
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-10-18 07:12:38 +0000

    Update to gitignore

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -3,4 +3,10 @@
 //  KrasamoStockTracker
 //
 //  Created by Antonio Mayorga on 2/5/19.
-//  Copyright © 2019 Rays Industrial Computers. All
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApp
