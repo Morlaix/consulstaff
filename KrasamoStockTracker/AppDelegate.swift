fe071cfc2a6a537ee447434fa431137cb41488de
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-10-21 14:34:55 +0000

    Update category.php

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -14,4 +14,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UI
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey:
