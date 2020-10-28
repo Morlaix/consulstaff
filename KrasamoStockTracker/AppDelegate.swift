43c222a291a97a956a00822c6e6a358f1de9d46a
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-10-28 19:36:12 +0000

    normalize signals/slots

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -15,4 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application l
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: U
