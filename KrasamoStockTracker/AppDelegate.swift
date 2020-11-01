1ea44f57058d1ab96f3a1110dee25bc17c64eab8
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-11-01 17:46:30 +0000

    [DOC] Working on matrix doc

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -20,4 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the
