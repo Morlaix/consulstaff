d53447c8d0605f0fcc9359cef83875eed8e828bd
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-12-20 06:07:28 +0000

    100% code coverage

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -33,4 +33,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeAct
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were p
