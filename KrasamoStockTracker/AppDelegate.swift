297af1b2598ddc8c5bd027a9a930a48621a373f7
Morlaix <91073342+Morlaix@users.noreply.github.com>
2020-12-27 08:54:46 +0000

    Updated the license

--- KrasamoStockTracker/AppDelegate.swift
+++ KrasamoStockTracker/AppDelegate.swift
@@ -38,4 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDid
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
