7eb9189d8ec5c9039eb0d92f30abb422a7105a78
Morlaix <91073342+Morlaix@users.noreply.github.com>
2021-03-21 10:38:49 +0000

    Update draft.md

--- KrasamoStockTracker/Controllers/StockViewController.swift
+++ KrasamoStockTracker/Controllers/StockViewController.swift
@@ -3,4 +3,11 @@
 //  KrasamoStockTracker
 //
 //  Created by Antonio Mayorga on 2/5/19.
-//  Copyright © 2019 Rays Industrial Comput
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+    
+    @
