4cf23016143600108f5ecad5fb661d7717dbbe79
Morlaix <91073342+Morlaix@users.noreply.github.com>
2021-08-23 08:34:58 +0000

    working on em_two_webpage

--- KrasamoStockTracker/Network/IEXClient.swift
+++ KrasamoStockTracker/Network/IEXClient.swift
@@ -3,4 +3,11 @@
 //  KrasamoStockTracker
 //
 //  Created by Antonio Mayorga on 2/5/19.
-//  Copyright © 2019 Rays Industrial C
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import Foundation
+
+class IEXClient {
+    
+    func getQuote(For
