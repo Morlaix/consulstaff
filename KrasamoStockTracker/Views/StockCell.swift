7a33c13a3f578974602c88dbc2ace195522f3ff2
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-02-20 18:31:32 +0000

    start over

--- KrasamoStockTracker/Views/StockCell.swift
+++ KrasamoStockTracker/Views/StockCell.swift
@@ -4,3 +4,10 @@
 //
 //  Created by Antonio Mayorga on 2/5/19.
 //  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+
+    @IBOutlet weak var st
