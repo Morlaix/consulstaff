9b58792de4f367e0f361c3315422ade9f7c103d7
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-03-03 13:11:59 +0000

    Some UI changes

--- KrasamoStockTracker/Views/StockCell.swift
+++ KrasamoStockTracker/Views/StockCell.swift
@@ -42,4 +42,7 @@ class StockCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the
+        // Configure the view for the selected state
+    }
+
+}
