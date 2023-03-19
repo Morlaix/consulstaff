1487c30156950d20bbf5acbf182634a636376e35
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-03-19 12:06:08 +0000

    parameter

--- KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
+++ KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
@@ -3,4 +3,8 @@
 //  KrasamoStockTrackerTests
 //
 //  Created by Antonio Mayorga on 2/5/19.
-//  Copyright © 2019 R
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import XCTest
+@testable import KrasamoStockTracker
