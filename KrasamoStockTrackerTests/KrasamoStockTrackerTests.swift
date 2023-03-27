96b639d992eb17dcec2806227d9122b633da4b07
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-03-27 21:49:13 +0000

    some merge

--- KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
+++ KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
@@ -21,4 +21,8 @@ class KrasamoStockTrackerTests: XCTestCase {
 
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce th
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        /
