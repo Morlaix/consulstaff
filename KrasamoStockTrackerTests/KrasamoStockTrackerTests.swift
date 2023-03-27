f628fbcff60a26d2b3b68631fd50deca660abcf3
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-03-27 11:59:36 +0000

    - перемещение

--- KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
+++ KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
@@ -18,3 +18,6 @@ class KrasamoStockTrackerTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
+    func testExample() {
+        // This is an example of a functional test case
