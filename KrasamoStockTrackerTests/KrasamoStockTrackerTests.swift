901fb43e8f0aca219292e448b9403b5dd1d88e6f
Morlaix <91073342+Morlaix@users.noreply.github.com>
2023-04-02 18:39:58 +0000

    Rapport

--- KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
+++ KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
@@ -27,4 +27,8 @@ class KrasamoStockTrackerTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-     
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
